5743c31eeba84324f3c6ac56288db589f5898dbe
Maksudul <maaksuduldish@gmail.com>
2025-03-06 11:10:54 +0600

    Mark as read functionality added

--- js/script.js
+++ js/script.js
@@ -9,13 +9,12 @@ const displayAllPost = (allPost) => {
     const allPostContainer = document.getElementById('all-post-container');
 
     for (const post of allPost) {
-        // console.log(post);
         const allPostCard = document.createElement('div');
         allPostCard.classList = `lg:flex bg-gray-100 p-5 rounded-xl hover:bg-purple-100 mb-5`;
         allPostCard.innerHTML = `
                     <div>
                         <img class="w-1/3 rounded-lg" src="${post.image}" alt="">
-                        <img class="-mt-18 mx-12 lg:-mt-20 lg:mx-15" src="images/Status.svg" alt="">
+                        <img class="-mt-18 mx-12 lg:-mt-20 lg:mx-15" src="images/green.png" alt="">
                     </div>
                     <div class="lg:space-y-2 mt-20 lg:mt-0 mr-10">
                         <div class="text-gray-600 text-xs font-normal flex gap-4">
@@ -41,12 +40,42 @@ const displayAllPost = (allPost) => {
                                 </div>
                             </div>
                             <div>
+                            <button class="read-button" data-id="${post.id}">
                                 <img class="lg:ml-64" src="images/read.png" alt="">
+                            </button>    
                             </div>
                         </div>
                     </div>
     `;
         allPostContainer.appendChild(allPostCard);
+        // const addToRead = document.getElementById('read-button').addEventListener('click',function(){
+        //     console.log('clicked')
+        // })
+        document.querySelectorAll('.read-button').forEach(button => {
+            button.addEventListener('click', function () {
+                const postId = this.getAttribute('data-id');
+                console.log(postId);
+                const readBookContainer = document.getElementById('read-book-container');
+                const readBookList = document.createElement('div');
+                readBookList.classList = `lg:flex md:flex justify-between items-center mt-3 bg-white p-5 rounded-lg`;
+                readBookList.innerHTML = `
+                    <div class="w-2/3">
+                            <h4 class="font-normal text-sm">${post.title}</h4>
+                        </div>
+                        <div class="flex gap-1 items-center">
+                            <img src="images/views.png" alt="">
+                            <p class="text-gray-600 text-sm">${post.view_count}</p>
+                        </div>
+                `;
+                readBookContainer.appendChild(readBookList);
+
+                const listedBooks = document.getElementById('listed-books');
+                const newListedBook = listedBooks.innerText;
+                const selectedBookNumber = parseInt(newListedBook);
+                const updatedList = selectedBookNumber + 1;
+                listedBooks.innerText = updatedList;
+            })
+        })
     }
 }
 
@@ -65,7 +94,6 @@ const displayLatestPosts = (latestPosts) => {
     const latestPostContainer = document.getElementById('latest-post-container');
 
     for (const latestPost of latestPosts) {
-        console.log(latestPost);
         const latestPostCard = document.createElement('div');
         latestPostCard.classList = `border border-gray-300 rounded-xl p-5 hover:bg-purple-100`;
         latestPostCard.innerHTML = `
